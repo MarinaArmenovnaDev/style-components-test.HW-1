583c8ff91f759135beed3a342e59a16f37dfe232
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2024-12-06 04:09:03 +0300

    fix img

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,39 @@
 import React from "react";
-import { styled } from "styled-components";
-import { Paragraf, TextTitle } from "./components/styles/TextTitle.styled";
-import { Img } from "./components/styles/Img.styled";
-import { Btn } from "./components/styles/Btn.styled";
-import { myTheme } from "./components/styles/Theme.styled";
+import {styled} from "styled-components";
+import {Paragraf, TextTitle} from "./components/styles/TextTitle.styled";
+import {MainImage} from "./components/styles/Img.styled";
+import {Btn} from "./components/styles/Btn.styled";
+import {myTheme} from "./components/styles/Theme.styled";
+import img from "../src/assets/img/0cbd17e4381497547009ce81acc4eee3.jpeg";
 
 function App() {
-  return (
-    <div>
-      <Box color={"#ffffff"}>
-        <Img
-          src="https://ixbt.online/live/images/original/20/93/55/2022/08/11/3d1b18dc51.png"
-          alt="breez"
-        />
-        <TextTitle color={"#000"}>Headline</TextTitle>
-        <Paragraf color={"#abb3ba"}>
-          Faucibus. Faucibus. Sit sit sapien sit <br /> tempusrisu ut. Sit
-          molestie ornare in venen.
-        </Paragraf>
-        <Btn color={myTheme.colors.primary} btnType={"primary"}>See more</Btn>
-        <Btn color={myTheme.colors.secondary} btnType={"outlined"}>Save</Btn>
-      </Box>
-    </div>
-  ); 
+    return (
+        <div>
+            <Box color={"#ffffff"}>
+                <MainImage src={img}/>
+                <TextTitle color={"#000"}>Headline</TextTitle>
+                <Paragraf color={"#abb3ba"}>
+                    Faucibus. Faucibus. Sit sit sapien sit <br/> tempusrisu ut. Sit
+                    molestie ornare in venen.
+                </Paragraf>
+                <Btn color={myTheme.colors.primary} btnType={"primary"}>See more</Btn>
+                <Btn color={myTheme.colors.secondary} btnType={"outlined"}>Save</Btn>
+            </Box>
+        </div>
+    );
 }
 
 type BoxPropsType = {
-  color: string;
+    color: string;
 };
 
 export const Box = styled.div<BoxPropsType>`
-  border-radius: ${myTheme.border.bordrRadius};
-  width: 300px;
-  height: 350px;
-  box-shadow: 0 4px 20px 5px rgba(0, 0, 0, 0.1);
-  background: ${(props) => props.color};
-  margin: 0 auto;
+    border-radius: ${myTheme.border.bordrRadius};
+    width: 300px;
+    height: 350px;
+    box-shadow: 0 4px 20px 5px rgba(0, 0, 0, 0.1);
+    background: ${(props) => props.color};
+    margin: 0 auto;
 `;
 
 export default App;
--- src/components/styles/Img.styled.tsx
+++ src/components/styles/Img.styled.tsx
@@ -1,10 +1,10 @@
-import { styled } from "styled-components";
-import { myTheme } from "./Theme.styled";
+import {styled} from "styled-components";
+import {myTheme} from "./Theme.styled";
 
-export const Img = styled.img`
-border-radius: ${myTheme.img.bordrRadius};
-width: 280px;
-height: 170px;
-margin: 10px;
-
+export const MainImage = styled.img`
+    border-radius: ${myTheme.img.bordrRadius};
+    width: 280px;
+    height: 170px;
+    margin: 10px;
+    object-fit: cover;
 `;
